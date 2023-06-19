3fbb0b59e187ac8ecd648f11fc5d1a88c2d8ef00
Mateo <frisded555@gmail.com>
2023-06-19 15:21:45 -0500

    Element position respect itself function created

--- src/components/gameHands/Paper.jsx
+++ src/components/gameHands/Paper.jsx
@@ -1,25 +1,44 @@
-import { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
 import { DataContext } from "../../context/DataContext";
-
 import paper from "../../assets/icon-paper.svg";
 
 function Paper() {
-    const { translateElement } = useContext(DataContext);
+    const { translateElement, sizeElement } = useContext(DataContext);
+
+    const [elementWidth, setElementWidth] = useState(0);
+    const [elementHeight, setElementHeight] = useState(0);
+
+    useEffect(() => {
+        const handleSizeElement = () => {
+            const width = sizeElement("paper", "x", 50);
+            const height = sizeElement("paper", "y", 50);
+            setElementWidth(width);
+            setElementHeight(height);
+        };
+
+        handleSizeElement();
+    }, [sizeElement]);
+
+    useEffect(() => {
+        document
+            .getElementById("paper")
+            .style.setProperty("top", `-${elementHeight}px`);
+        document
+            .getElementById("paper")
+            .style.setProperty("left", `-${elementWidth}px`);
+    }, [elementHeight, elementWidth]);
 
     const handleOnClick = (e) => {
-        console.log(e);
-        translateElement(0, 100, "triangle", e.target.id);
+        translateElement(0, 0, "triangle", e.target.id);
     };
 
-
-    // Crear una función en javascript que permita posicionar un objeto con las propiedades de position respecto a su tamaño
     return (
         <button
             id="paper"
             onClick={(e) => {
                 handleOnClick(e);
             }}
-            className="absolute top-0 translate-x-[-50%] translate-y-[-50%] hand paper"
+            className="absolute hand paper"
         >
             <div id="paper" className="innerHand">
                 <img id="paper" src={paper} alt="Paper" />
--- src/context/DataContext.jsx
+++ src/context/DataContext.jsx
@@ -3,35 +3,54 @@ import { createContext, useState } from "react";
 export const DataContext = createContext();
 
 export const DataProvider = ({ children }) => {
+    const [score, setScore] = useState(12);
 
-  const [score, setScore] = useState(12)
+    const [result, setResult] = useState(true);
 
-  const [result, setResult] = useState(true)
+    function translateElement(x, y, containerId, elementId, duration = "1s") {
+        const container = document.getElementById(containerId);
+        const element = document.getElementById(elementId);
 
-  function translateElement(x, y, containerId, elementId, duration = "1s") {
-    const container = document.getElementById(containerId);
-    const element = document.getElementById(elementId);
-  
-    const containerRect = container.getBoundingClientRect();
-    const elementRect = element.getBoundingClientRect();
-  
-    // Calcula las coordenadas de desplazamiento respecto al contenedor
-    const offsetX = (containerRect.width * x) / 100 - (elementRect.left - containerRect.left);
-    const offsetY = (containerRect.height * y) / 100 - (elementRect.top - containerRect.top);
-  
-    // Aplica la transformación de traducción al elemento
-    element.style.transitionDuration = duration;
-    element.style.transform = `translate(${offsetX}px, ${offsetY}px)`;
-  }
+        const containerRect = container.getBoundingClientRect();
+        const elementRect = element.getBoundingClientRect();
 
-    return <DataContext.Provider
-    // value es un objeto en el que puedo compartir lo que deseo
-    value={{
-      score,
-      setScore,
-      result,
-      setResult,
-      translateElement
-    }}
-    >{children}</DataContext.Provider>;
+        // Calcula las coordenadas de desplazamiento respecto al contenedor
+        const offsetX =
+            (containerRect.width * x) / 100 -
+            (elementRect.left - containerRect.left);
+        const offsetY =
+            (containerRect.height * y) / 100 -
+            (elementRect.top - containerRect.top);
+
+        // Aplica la transformación de traducción al elemento
+        element.style.transitionDuration = duration;
+        element.style.transform = `translate(${offsetX}px, ${offsetY}px)`;
+    }
+
+    // sizeElement sirve para obtener el porcentage del tamaño de un elemento
+    function sizeElement(elementId, axis, percentage) {
+        const element = document.getElementById(elementId);
+
+        if (axis == "x") {
+            return (element.getBoundingClientRect().width * percentage) / 100;
+        } else if (axis == "y") {
+            return (element.getBoundingClientRect().height * percentage) / 100;
+        }
+    }
+
+    return (
+        <DataContext.Provider
+            // value es un objeto en el que puedo compartir lo que deseo
+            value={{
+                score,
+                setScore,
+                result,
+                setResult,
+                translateElement,
+                sizeElement,
+            }}
+        >
+            {children}
+        </DataContext.Provider>
+    );
 };
